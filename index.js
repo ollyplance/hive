f03f99423127948ff8000da874fa621ef27dfa3d
Oliver Lance <42589078+ollylance@users.noreply.github.com>
2023-10-11 18:44:52 -0700

    hexagon grid with panning

--- index.js
+++ index.js
@@ -1,102 +1,116 @@
-let game;
-let gameOptions = {
+import {
+    DoubledCoord,
+    Hex,
+    Layout,
+    OffsetCoord,
+    Orientation,
+    Point,
+} from './js/hexgrid.js';
 
-    // water level, in % of screen height
-    waterLevel: 50,
+class playGame extends Phaser.Scene {
+    constructor() {
+        super();
+    }
 
-    // ball gravity
-    ballGravity: 1600,
+    preload() {
+        // Load assets
+    }
 
-    // jump power
-    ballPower: 800
-}
+    create() {
+        let { width, height } = this.sys.game.canvas;
+        var hexagons = this.add.container();
+        var hexSize = 30;
+        const numRows = 28;
+        const numCols = 28;
 
-window.onload = function() {
-    let gameConfig = {
-        type: Phaser.AUTO,
-        backgroundColor:0x888888,
-        scale: {
-            mode: Phaser.Scale.FIT,
-            autoCenter: Phaser.Scale.CENTER_BOTH,
-            parent: "canvas-container",
-            width: 750,
-            height: 1334
-        },
-        physics: {
-            default: "arcade",
-            arcade: {
-                gravity: {
-                    y: 0
-                }
+        var cam = this.cameras.main;
+        
+        this.input.mousePointer.motionFactor = 0.5;
+        this.input.pointer1.motionFactor = 0.5;
+
+        var orientation = Layout.flat;
+        var layout = new Layout(orientation, new Point(hexSize, hexSize), new Point(hexSize*2, hexSize*2));
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+
+        for (var row = 0; row < numRows; row++) {
+            for (var col = 0; col < numCols; col++) {
+                var cube = OffsetCoord.qoffsetToCube(OffsetCoord.ODD, new OffsetCoord(row, col));
+                var corners = layout.polygonCorners(cube);
+                const hexagon = this.add.polygon(0, 0, corners);
+
+                hexagon
+                    .setData('row', row)
+                    .setData('col', col)
+                    .setInteractive(new Phaser.Geom.Polygon(corners),
+                                        Phaser.Geom.Polygon.Contains)
+                    .setStrokeStyle(2, 0xFF80BF, .5)
+                    .on('pointerover', () => {
+                        hexagon.setFillStyle(0xCC99FF, .5);
+                    })
+                    .on('pointerout', () => {
+                        hexagon.setFillStyle(0xCCCCCC);
+                    })
+                    .on('pointerdown', function () {
+                        const hexRow = hexagon.getData('row');
+                        const hexCol = hexagon.getData('col');
+                        console.log(`Clicked Hexagon at Row: ${hexRow}, Col: ${hexCol}`);
+                    });
+
+                hexagons.add(hexagon);
             }
-        },
-        scene: playGame
-    }
-    game = new Phaser.Game(gameConfig);
-    window.focus();
-}
+        }
+        console.log(hexagons)
+        cam.setBounds(0, 0, hexSize * 2 * (numCols-1), hexSize * 2 * (numRows-1));
+        cam.setViewport(0, 0, 720, 720);
+        cam.centerOn((numCols * hexSize)/2, (numRows * hexSize)/2)
+        cam.setZoom(1);
 
-class playGame extends Phaser.Scene{
-    constructor(){
-        super("PlayGame");
-    }
+        // help from https://codepen.io/samme/pen/XWJxgRG
+        this.input.on("pointermove", function (p) {
+            if (!p.isDown) return;
 
-    preload(){
-        this.load.image("ball", "assets/ball.png");
-        this.load.image("water", "assets/water.png");
-    }
+            const { x, y } = p.velocity;
 
-    create(){
-
-        // add water sprite
-        this.water = this.add.sprite(0, game.config.height / 100 * gameOptions.waterLevel, "water");
-
-        // set registration point to top left pixel
-        this.water.setOrigin(0, 0);
-
-        // set display width to cover the entire game width
-        this.water.displayWidth = game.config.width;
-
-        // set display height to cover from water level to the bottom of the canvas
-        this.water.displayHeight = game.config.height - this.water.y;
-
-        // add ball sprite
-        this.ball = this.physics.add.sprite(game.config.width / 2, game.config.height / 4, "ball");
-
-        // set ball ballGravity
-        this.ball.body.gravity.y = gameOptions.ballGravity * (this.isUnderwater() ? -1 : 1)
-
-        // listener for input, calls "jump" method
-        this.input.on("pointerdown", this.jump, this);
+            cam.scrollX -= x / cam.zoom;
+            cam.scrollY -= y / cam.zoom;
+        });
     }
 
-    // method to make ball jump
-    jump(){
+    update() {
+        const cam = this.cameras.main;
 
-        // set ball velocity to positive or negative jump speed according to ball position
-        this.ball.body.velocity.y = gameOptions.ballPower * (this.isUnderwater() ? 1 : -1);
-    }
+        if (this.cursors.left.isDown)
+        {
+            cam.scrollX -= 4;
+            console.log(cam)
+        }
+        else if (this.cursors.right.isDown)
+        {
+            cam.scrollX += 4;
+        }
 
-    // method to check if the ball is underwater
-    isUnderwater(){
-
-        // true if ball y position is higher than water level
-        return this.ball.y > game.config.height / 100 * gameOptions.waterLevel
-    }
-    update(){
-
-        // determine next ball gravity
-        let nextGravity = gameOptions.ballGravity * (this.isUnderwater() ? -1 : 1);
-
-        // is next ball gravity different than current ball gravity?
-        // this means we moved from land to water or from water to land
-        if(nextGravity != this.ball.body.gravity.y){
-
-            // set ball ballGravity
-            this.ball.body.gravity.y = nextGravity;
-
-            // set ball velocity as if we just jumped
-            this.ball.body.velocity.y = gameOptions.ballPower * (this.isUnderwater() ? 1 : -1);
+        if (this.cursors.up.isDown)
+        {
+            cam.scrollY -= 4;
+        }
+        else if (this.cursors.down.isDown)
+        {
+            cam.scrollY += 4;
         }
     }
 }
+
+let gameConfig = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    backgroundColor: 0xFFFFFF,
+    scale: {
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        parent: "canvas-container",
+    },
+    scene: playGame,
+};
+
+var game = new Phaser.Game(gameConfig);
